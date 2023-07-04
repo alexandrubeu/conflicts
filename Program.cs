c521251a651c0f1806eaa20fff2b0a31fed4912e
Igor L <lungu6002@gmail.com>
2023-07-04 12:11:41 +0300

    Nume prenume

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 using System;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan");
+
 Console.WriteLine("Numele meu este Eduard");
+
 Console.WriteLine("Cristi");
+
+
+Console.WriteLine("Lungu Igor");
+
+
