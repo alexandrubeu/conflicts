19a40853a506e1af0ff232c0cb531fa8061b0ce3
unknown <brainomato@gmail.com>
2023-07-04 11:57:52 +0300

    Please Work

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan")
+Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan");
+Console.WriteLine("Cristi");
