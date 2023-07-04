a4341bed86c199e9212e5a7bc9a9da60aea24dcf
EdiTira <eduard.tira@ulbsibiu.ro>
2023-07-04 11:56:37 +0300

    Nume adaugat

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan")
+Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan");
+Console.WriteLine("Numele meu este Eduard");
