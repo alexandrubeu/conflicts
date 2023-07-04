31e720a72a7eba8fe523363ad22111b7e8afcffb
Chirtop Catalin <catalinioan.chirtop@ulbsibiu.ro>
2023-07-04 12:47:57 +0300

    say my name

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan");
+Console.WriteLine("Catalin");
 Console.WriteLine("Numele meu este Eduard");
