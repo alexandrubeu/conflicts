9e44ad3a9e362a5cda1231734347c902143070d9
Elitenoob <98418212+Elitenoob3@users.noreply.github.com>
2023-07-04 12:08:18 +0300

    Please Work (#7)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ma cheama Alex dar colegii de la seriviciu ma numesc Dan");
 Console.WriteLine("Numele meu este Eduard");
+Console.WriteLine("Cristi");
