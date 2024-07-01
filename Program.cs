83eb11b7153ea4b8b898b2cf4e46a2499ccc9e81
iiiulia12 <ratiuiulia@yahoo.com>
2024-07-01 10:58:15 +0300

    add my name - Iulia

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// very important cepoiu feature
-var a = 5;
-a++;
+Console.WriteLine("Iuliaa");
