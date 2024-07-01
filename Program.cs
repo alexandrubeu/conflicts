4bec0eb0dc694fe770cee1257d779e17e84ffdeb
iiiulia12 <ratiuiulia@yahoo.com>
2024-07-01 11:37:35 +0300

    resolve conflicts again

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("Olaru Andrei-Costin");
 Console.WriteLine("Cristian");
+=======
+<<<<<<< HEAD
+>>>>>>> cf09de8 (resolve name conflict)
 Console.WriteLine("Hello, World!");
 
 // very important cepoiu feature
 var a = 5;
 a++;
+<<<<<<< HEAD
 
 Console.WriteLine("hello, Cosmin is here");
 
 
 //cool message
+=======
+=======
+Console.WriteLine("Iuliaa");
+>>>>>>> 562cba0 (add my name - Iulia)
+>>>>>>> cf09de8 (resolve name conflict)
