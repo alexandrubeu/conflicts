1ecc6c912900c66e9b4d5a6703d97e8ab28d7973
iiiulia12 <119032247+iiiulia12@users.noreply.github.com>
2024-07-01 11:52:13 +0300

    add my name - Iulia (#7)
    * resolve conflicts again
    
    * resolve conflicts again

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
+<<<<<<< HEAD
 Console.WriteLine("Olaru Andrei-Costin");
 Console.WriteLine("Cristian");
+=======
+<<<<<<< HEAD
+>>>>>>> cf09de8 (resolve name conflict)
+=======
+
+>>>>>>> 3cf3035 (Resolve conflict)
 Console.WriteLine("Hello, World!");
 
 // very important cepoiu feature
 var a = 5;
 a++;
+<<<<<<< HEAD
+<<<<<<< HEAD
 
 Console.WriteLine("hello, Cosmin is here");
 
 
 //cool message
+=======
+=======
+Console.WriteLine("Iuliaa");
+>>>>>>> 562cba0 (add my name - Iulia)
+>>>>>>> cf09de8 (resolve name conflict)
+=======
+
+Console.WriteLine("Iuliaa");
+
+>>>>>>> 3cf3035 (Resolve conflict)
