5b58a3a0aa4cab5bcac06f61b81c71a41bda77a1
Con Nik <conn.nik1@gmail.com>
2024-07-01 11:40:08 +0300

    Niky is here

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("hello, Niky is here");
 // very important cepoiu feature
 var a = 5;
 a++;
 
+Console.WriteLine("hello, Niky is here");
 Console.WriteLine("hello, Cosmin is here");
 
+Console.WriteLine("hello, Niky is here");
+Console.WriteLine("hello, Niky is here");
